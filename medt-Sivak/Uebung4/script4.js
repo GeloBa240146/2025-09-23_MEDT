7d71d778bb29c3027d320d814a8c5b08d9b2a0c7
Baris Geloyan <40146720240146@litec.ac.at>
2025-12-02 14:59:58 +0100

    feat: exercise finished

--- medt-Sivak/Uebung4/script4.js
+++ medt-Sivak/Uebung4/script4.js
@@ -1,18 +1,34 @@
-let start = prompt("Geben Sie den Start ein:");
-let end = prompt("Geben Sie die letzte zahl ein:");
-
-let wert1;
-let wert2;
+let startIdx;
+let endIdx;
 
 do {
-    wert1 = prompt("Geben Sie den Start ein:");
-    wert2 = prompt("Geben Sie die letzte zahl ein:");
-} while(wert2 < wert1);
+    startIdx = parseInt(prompt("Geben Sie den Start ein: "));
+    endIdx = parseInt(prompt("Geben Sie das Ende ein: "));
+} while (startIdx > endIdx);
 
+let list = document.createElement("ul");
 
-
-for (let i = 0; i >= start && i <= end; i++) {
-    if (i % 2 == 0) {
-        i;
+for (let i = startIdx; i <= endIdx; i++) {
+    if(isPrime(i)) {
+        // Dann Zahl ausgeben
+        let primeNumber = document.createElement("li");
+        primeNumber.innerText = i;
+        
+        list.appendChild(primeNumber);
     }
+}
+
+document.body.appendChild(list);
+
+function isPrime(number) {
+    if (number <= 1 ) {
+        return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
